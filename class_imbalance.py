215610600b0dcf718562bd2c7e0310b6989dc1cf
Oyinlola <babsoyinlola@yahoo.com>
2018-12-11 11:25:26 +0000

    wrong stash pop

---
+++ class_imbalance.py
@@ -0,0 +1,395 @@
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.decomposition import PCA
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+class ClassImbalance:
+    def __init__(self):
+
+    def read_data(self, filepath):
+        colums = ["Sex","Length","Diameter", "Height", "Whole_weight", "Shucked_weight", "Viscera_weight", 
+                    "Shell_weight", "Class"]
+        data = pd.read_csv(filepath, names=colums)
+        data = pd.DataFrame(data)
+
+        self.data = np.array(data)
+        for i in range(self.data.shape[0]):
+            self.data[i, 8] = self.data[i, 8].strip()
+    
+        return
+
+    #set k = 4174 - 32 = 4142 (positives = 2)
+    def pre_process_undersample(self, k, label):
+
+        undersampled_data = np.array([])
+        label_index = []
+
+        #select indexes of rows with specified label
+        for row in range(self.data.shape[0]):
+            if self.data[row, 8] == label:
+                label_index.append(row)
+
+        random_remove = random.sample(label_index, k)
+
+        for i in range(self.data.shape[0]):
+            if i not in random_remove:
+                if len(undersampled_data) == 0:
+                    undersampled_data = self.data[i, :].copy()
+                else:
+                    undersampled_data = np.vstack((undersampled_data, self.data[i, :]))
+
+        return undersampled_data
+
+    def pre_process_oversample(self, k, label):
+        oversampled_data = self.data.copy()
+        label_index = []
+
+        for row in range(self.data.shape[0]):
+            if self.data[row, 8] == label:
+                label_index.append(row)
+
+        for i in range(k):
+            index = label_index[random.randint(0, (len(label_index)-1))]
+            item = self.data[index]
+
+            oversampled_data = np.vstack((oversampled_data, item))
+        
+        return oversampled_data
+
+    def euclidean_distance(self, point1, point2):
+        total = 0
+        for i in range(len(point1)):
+            total = total + pow(point1[i] + point2[i], 2)
+
+        return math.sqrt(total)
+
+    #Fix this
+    def smote(self, k, label):
+        data = self.data.copy()
+        label_index = []
+
+        for row in range(self.data.shape[0]):
+            if self.data[row, 8] == label:
+                label_index.append(row)
+        
+        for i in range(k):
+            rand_item = label_index[random.randint(0, (len(label_index) - 1))]
+
+            #KNN
+            distances = []
+            distance_index = []
+
+            for item in label_index:
+                if item == rand_item:
+                    continue
+                
+                distances.append(self.euclidean_distance(self.data[item], self.data[rand_item]))
+                distance_index.append(item)
+
+            k_neigh = []
+
+            for neigh in range(2):
+                nearest = np.argmin(distances)
+                k_neigh.append(distance_index[nearest])
+                distances[nearest] = float("inf")
+            
+            neigh_rand = k_neigh[random.randint(0, (len(k_neigh) - 1))]
+
+            feat = []
+            for j in range(7):
+                col = self.data[neigh_rand, j] - self.data[rand_item, j]
+                feat.append(col)
+            
+            alpha = random.random()
+
+            all_feat = []
+
+            for j in range(len(feat)):
+                col = self.data[rand_item, j] + alpha*feat[j]
+                all_feat.append(col)
+            
+            all_feat.append(label)
+
+            data = np.vstack((data, all_feat))
+
+        return data
+
+    #binary classification
+    def logistic_regression_oversampled(self):
+        data = self.pre_process_oversample(4110, "positive")
+
+        labels  = data[:, 8]
+        features =  np.delete(data, obj=8, axis=1)
+
+        #one hot encode sex since
+        new_col = pd.get_dummies(features[:, 0])
+        #create new columns for sex class
+        new_col = np.array(new_col)
+        #add the new columns to features
+        features = np.column_stack([features, new_col])
+        #delete sex column 
+        features =  np.delete(features, obj=0, axis=1)
+
+        x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3)
+
+        reg = LogisticRegression()
+        reg.fit(x_train, y_train)
+        pred = reg.predict(x_test)
+
+        accuracy = metrics.accuracy_score(y_test, pred)
+        print("Logisitic Regression - Accuracy over sampled data without PCA: ", accuracy)
+        print()
+        return
+
+    def logistic_regression_undersampled(self):
+        data = self.pre_process_undersample(4110, "negative")
+
+        labels  = data[:, 8]
+        features =  np.delete(data, obj=8, axis=1)
+
+        #one hot encode sex
+        new_col = pd.get_dummies(features[:, 0])
+        #create new columns for sex class
+        new_col = np.array(new_col)
+        #add the new columns to features
+        features = np.column_stack([features, new_col])
+        #delete sex column 
+        features =  np.delete(features, obj=0, axis=1)
+
+        x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3)
+
+        reg = LogisticRegression()
+        reg.fit(x_train, y_train)
+        pred = reg.predict(x_test)
+
+        accuracy = metrics.accuracy_score(y_test, pred)
+        print("Logisitic Regression - Accuracy under sampled data without PCA: ", accuracy)
+        print()
+        return
+
+    def logistic_regression_oversampled_PCA(self):
+        data = self.pre_process_oversample(4110, "positive")
+
+        labels  = data[:, 8]
+        features =  np.delete(data, obj=8, axis=1)
+
+        #one hot encode sex since
+        new_col = pd.get_dummies(features[:, 0])
+        #create new columns for sex class
+        new_col = np.array(new_col)
+        #add the new columns to features
+        features = np.column_stack([features, new_col])
+        #delete sex column 
+        features =  np.delete(features, obj=0, axis=1)
+
+        variance = self.percentage_of_variance(features, "Abalone")  
+        # print("Feature variances: ", variance)
+        #this shows that only the length, diameter and height contribute a greater percentage
+        #drop other features
+
+        features = self.PCA(features, 5)
+
+        x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3)
+
+        reg = LogisticRegression()
+        reg.fit(x_train, y_train)
+        pred = reg.predict(x_test)
+
+        accuracy = metrics.accuracy_score(y_test, pred)
+        print("Logisitic Regression - Accuracy over sampled data after PCA: ", accuracy)
+        print()
+        return
+
+    def logistic_regression_undersampled_PCA(self):
+        data = self.pre_process_undersample(4110, "negative")
+
+        labels  = data[:, 8]
+        features =  np.delete(data, obj=8, axis=1)
+
+        #one hot encode sex
+        new_col = pd.get_dummies(features[:, 0])
+        #create new columns for sex class
+        new_col = np.array(new_col)
+        #add the new columns to features
+        features = np.column_stack([features, new_col])
+        #delete sex column 
+        features =  np.delete(features, obj=0, axis=1)
+
+        variance = self.percentage_of_variance(features, "Abalone")  
+        # print("Feature variances: ", variance)
+        #this shows that only the length, diameter and height contribute a greater percentage
+        #drop other features
+
+        features = self.PCA(features, 5)
+
+        x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3)
+
+        reg = LogisticRegression()
+        reg.fit(x_train, y_train)
+        pred = reg.predict(x_test)
+
+        accuracy = metrics.accuracy_score(y_test, pred)
+        print("Logisitic Regression - Accuracy under sampled data after PCA: ", accuracy)
+        print()
+        return
+
+    def decision_tree_oversampled(self):
+        data = self.pre_process_oversample(4110, "positive")
+
+        labels  = data[:, 8]
+        features =  np.delete(data, obj=8, axis=1)
+
+        #one hot encode sex since
+        new_col = pd.get_dummies(features[:, 0])
+        #create new columns for sex class
+        new_col = np.array(new_col)
+        #add the new columns to features
+        features = np.column_stack([features, new_col])
+        #delete sex column 
+        features =  np.delete(features, obj=0, axis=1)
+
+        x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3)
+
+        tree = DecisionTreeClassifier()
+        tree.fit(x_train, y_train)
+        pred = tree.predict(x_test)
+
+        accuracy = metrics.accuracy_score(y_test, pred)
+        print("Decision tree - Accuracy over sampled data without PCA: ", accuracy)
+        print()
+        return
+
+    def decision_tree_undersampled(self):
+        data = self.pre_process_undersample(4110, "negative")
+
+        labels  = data[:, 8]
+        features =  np.delete(data, obj=8, axis=1)
+
+        #one hot encode sex
+        new_col = pd.get_dummies(features[:, 0])
+        #create new columns for sex class
+        new_col = np.array(new_col)
+        #add the new columns to features
+        features = np.column_stack([features, new_col])
+        #delete sex column 
+        features =  np.delete(features, obj=0, axis=1)
+
+        x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3)
+
+        tree = DecisionTreeClassifier()
+        tree.fit(x_train, y_train)
+        pred = tree.predict(x_test)
+
+        accuracy = metrics.accuracy_score(y_test, pred)
+        print("Decision tree - Accuracy under sampled data without PCA: ", accuracy)
+        print()
+        return
+
+    def decision_tree_oversampled_PCA(self):
+        data = self.pre_process_oversample(4110, "positive")
+
+        labels  = data[:, 8]
+        features =  np.delete(data, obj=8, axis=1)
+
+        #one hot encode sex since
+        new_col = pd.get_dummies(features[:, 0])
+        #create new columns for sex class
+        new_col = np.array(new_col)
+        #add the new columns to features
+        features = np.column_stack([features, new_col])
+        #delete sex column 
+        features =  np.delete(features, obj=0, axis=1)
+
+        variance = self.percentage_of_variance(features, "Abalone")  
+        # print("Feature variances: ", variance)
+        #this shows that only the length, diameter and height contribute a greater percentage
+        #drop other features
+
+        features = self.PCA(features, 5)
+
+        x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3)
+
+        tree = DecisionTreeClassifier()
+        tree.fit(x_train, y_train)
+        pred = tree.predict(x_test)
+
+        accuracy = metrics.accuracy_score(y_test, pred)
+        print("Decision tree - Accuracy over sampled data after PCA: ", accuracy)
+        print()
+        return
+
+    def decision_tree_undersampled_PCA(self):
+        data = self.pre_process_undersample(4110, "negative")
+
+        labels  = data[:, 8]
+        features =  np.delete(data, obj=8, axis=1)
+
+        #one hot encode sex
+        new_col = pd.get_dummies(features[:, 0])
+        #create new columns for sex class
+        new_col = np.array(new_col)
+        #add the new columns to features
+        features = np.column_stack([features, new_col])
+        #delete sex column 
+        features =  np.delete(features, obj=0, axis=1)
+
+        variance = self.percentage_of_variance(features, "Abalone")  
+        # print("Feature variances: ", variance)
+        #this shows that only the length, diameter and height contribute a greater percentage
+        #drop other features
+
+        features = self.PCA(features, 5)
+
+        x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3)
+
+        tree = DecisionTreeClassifier()
+        tree.fit(x_train, y_train)
+        pred = tree.predict(x_test)
+
+        accuracy = metrics.accuracy_score(y_test, pred)
+        print("Decision tree - Accuracy under sampled data after PCA: ", accuracy)
+        print()
+        return
+
+    def PCA(self, data, n):
+        pca = PCA(n_components=n)  #10 features (excluding sex and including encoded sex classes)
+        pca.fit(data)
+        cof = pca.components_
+        trasform_data = pca.transform(data)
+        return trasform_data
+
+    def percentage_of_variance(self, data, name):
+        pca = PCA(n_components=10)
+        pca.fit(data)
+        colums = ["Length","Diameter", "Height", "Whole_weight", "Shucked_weight", "Viscera_weight", 
+                    "Shell_weight", "Sex-F", "Sex-I", "Sex-M"]
+        plt.bar(colums, pca.explained_variance_ratio_, tick_label= colums)
+        plt.xlabel("Principal Component")
+        plt.ylabel("% Variance Explained")
+        plt.xticks(rotation='vertical')
+        plt.tight_layout()
+        plt.title("Percentage of Variance")   
+        plt.savefig("Percentage of variance " + name + ".jpeg")
+        # plt.show()
+        return pca.explained_variance_ratio_
+
+
+abalone = ClassImbalance()
+abalone.read_data("/Users/oyinlola/Desktop/MSc Data Science/SCC403 - Data Mining/Coursework/abalone19.csv")
+# undersampled = abalone.pre_process_undersample(4110, "negative") 
+# oversampled = abalone.pre_process_oversample(4110, "positive")
+
+abalone.logistic_regression_oversampled()
+abalone.logistic_regression_oversampled_PCA()
+abalone.logistic_regression_undersampled()
+abalone.logistic_regression_undersampled_PCA()
+abalone.decision_tree_oversampled()
+abalone.decision_tree_oversampled_PCA()
+abalone.decision_tree_undersampled()
+abalone.decision_tree_undersampled_PCA()
--- clustering.py
+++ clustering.py
@@ -1,4 +1,11 @@
+<<<<<<< Updated upstream
+=======
+import math
+import time
+import numpy as np
+>>>>>>> Stashed changes
 import scipy.cluster as sc
+import matplotlib.pyplot as plt
 import scipy.spatial.distance as sd
 import matplotlib.pyplot as plt
 import math
@@ -6,9 +13,14 @@ import numpy as np
 
 
 class HierachicalClustering:
+<<<<<<< Updated upstream
     def __init__(self):
         
         return
+=======
+    def __init__(self, data):
+        self.data = data
+>>>>>>> Stashed changes
     
     def distance(self, data):
 
@@ -30,6 +42,7 @@ class HierachicalClustering:
 
         return distanceMatrix
 
+<<<<<<< Updated upstream
     def calc(self, data):
         condenced_distance = sd.squareform(self.distance(data))
         linkage = sc.hierarchy.linkage(condenced_distance) #link points based on distance
@@ -39,59 +52,193 @@ class HierachicalClustering:
 
 
 
+=======
+    def calc(self):
+        distance = self.distance()
+        
+        #Calculate time:
+        start_time = time.process_time() #start time
 
-#KMeans using scipy lib
-# class KMeans:
-#     def __init__(self, k=3):
-#         self.k = k
-#         self.centroids = []
+        condenced_distance = sd.squareform(distance)
+        linkage = sc.hierarchy.linkage(condenced_distance) #link points based on distance
+        # sc.hierarchy.dendrogram(linkage)  
+        # plt.savefig("Location dendrogram without pruning")      
+        #prune the tree here
+        print("Prunning tree")
+        # sc.hierarchy.dendrogram(linkage, truncate_mode="lastp", p =0.2)
+        # plt.savefig("Location dendrogram prunned at p=0.25")  
+        sc.hierarchy.dendrogram(linkage, truncate_mode="level", p = 3)
+        plt.savefig("Location dendrogram prunned at level p=3") 
+        # sc.hierarchy.dendrogram(linkage, truncate_mode="lastp", p =0.1)
+        # plt.savefig("Location dendrogram prunned at p=0.15")
+>>>>>>> Stashed changes
 
-#     def cluster(self, data):
-#         self.centroids, distortion = sc.vq.kmeans(data, self.k)
+        end_time = time.process_time() - start_time 
+        print("Time for Hierarchical Clustering: ", end_time)
 
-#     def dist(self, p1, p2):
-#         sumTotal = 0
+        plt.show()
 
-#         for c in range(len(p1)):
-#             sumTotal = sumTotal + pow((p1[c] - p2[c]),2)
-
-#         return math.sqrt(sumTotal)
+        return
 
 
-#     def plot(self, data, title):
-#         self.cluster(data)
-#         plt.figure(figsize=(6,4))
-#         centroid_colours = ['mx', 'gx', 'yx', 'rx', 'bx', 'kx', 'wx']
-#         plt.plot(data[:,0],data[:,1],'.')
-       
-#         for i in range(self.k):
-#             plt.plot(self.centroids[i][0], self.centroids[0][1],centroid_colours[i])
+class KMeans:
 
-#         # groups = []
-#         # group1 = np.array([])
-#         # group2 = np.array([])
-#         # for i in range(self.k):
-#         #     groups.append(np.array([]))
+    def __init__ (self, k = 3):
+        try:
+            if k > 5 :
+                self.k = None
+                raise Exception("Maximum clusters is 5")
+            elif k < 1:
+                self.k = None
+                raise Exception("Minimum clusters is 2")
+            else:
+                self.k = k
+                self.groups = []
+                self.iter = 0
+        except Exception as error:
+            print(error)
 
-#         # for d in data:
-#         #     if (self.(d, self.centroids[0,:]) < self.dist(d, self.centroids[1,:])):
-#         #         if (len(group1) == 0):
-#         #             group1 = d
-#         #         else:
-#         #             group1 = np.vstack((group1,d))
-#         #     else:
-#         #         if (len(group2) == 0):
-#         #             group2 = d
-#         #         else:
-#         #             group2 = np.vstack((group2,d))
+    def initial_centroids(self, data):
+        #choose initial centroids
+        centroids = data[np.random.choice(data.shape[0], self.k, False)]
+        return centroids
 
-#         # plt.figure(figsize=(6,4))
+    def euclidean_distance(self, point1, point2):
+        total = 0
+        for i in range(len(point1)):
+            total = total + pow(point1[i] + point2[i], 2)
 
-#         # plt.plot(group1[:,0],group1[:,1],'r.')
-#         # plt.plot(group2[:,0],group2[:,1],'g.')
+        return math.sqrt(total)
 
+    def cluster(self, data, cent):
+        centroids = cent
+        self.centroids = centroids
+
+        grouped_points = [[] for _ in range(self.k)]  
+
+        for i in range(data.shape[0]):
+            dist_min = np.inf
+            label_index = -1
+
+            for j in range(self.k):
+                dist = self.euclidean_distance(data[i, :], centroids[j])
+                if(dist < dist_min):
+                    dist_min = dist
+                    label_index = j
+
+            grouped_points[label_index].append(data[i, :])
+        self.grouped_points = grouped_points
+        
+        return
+
+<<<<<<< Updated upstream
 #         # plt.plot(centroids[0,0],centroids[0,1],'rx')
 #         # plt.plot(centroids[1,0],centroids[1,1],'gx')
 #         # plt.title(title)
 #         # plt.show()
 
+=======
+    def new_centroid(self):
+        new_centroids = np.empty((self.k, 2))  
+        points = self.grouped_points
+
+        for i in range(self.k):
+            total_x = 0
+            total_y = 0
+
+            temp = points[i]
+            for j in range(len(points[i])):
+                total_x = total_x + temp[j][0]
+                total_y = total_y + temp[j][1]
+
+            new_centroids[i][0] = total_x /len(temp)
+            new_centroids[i][1] = total_y /len(temp)
+
+        return new_centroids
+
+    def centroid_diff(self, old_centroids, new_centroids):
+        dist = [0]*self.k
+
+        for i in range(self.k):
+            dist[i] = self.euclidean_distance(new_centroids[i], old_centroids[i])
+
+
+        difference = sum(dist)
+
+        return difference
+
+    def iterate(self, data):
+        try:
+            if self.k == None:
+                raise Exception("")
+                return
+            else:
+                #Calculate time:
+                start_time = time.process_time() #start time
+
+                centroid = self.initial_centroids(data)
+                self.cluster(data, centroid)
+
+                diff = 9999999999
+                threshold = 0.0001
+
+                count = 0
+                temp = 0
+                final_centroid = centroid
+
+                while(diff > threshold):
+                    new_centroid = self.new_centroid()
+                    diff_new = self.centroid_diff(centroid, new_centroid)
+                    self.cluster(data, new_centroid)
+                    
+                    if count > 0:
+                        diff = abs(diff_new - temp)
+
+                    print("Old centroid: ")
+                    print(centroid)
+                    print("New centroid: ")
+                    print(new_centroid)
+                    print("Difference: ", diff)
+
+                    centroid = new_centroid
+                    temp = diff_new
+                    count = count + 1
+                    final_centroid = new_centroid
+
+                end_time = time.process_time() - start_time 
+                print("Time for K-Means Clustering: ", end_time)
+
+                self.centroids = final_centroid
+
+                return
+        except Exception as error:
+            print(error)
+            
+    def plot(self, title):
+        try:
+            if self.k == None:
+                raise Exception("")
+                return
+            else:
+                plt.figure(figsize=(6,4))
+                col = ["blue", "red", "black", "brown", "green", "cyan"]
+                points = self.grouped_points
+                for i in range(len(points)):
+                    temp = points[i]
+                    for j in range(len(temp)):
+                        plt.plot(temp[j][0],temp[j][1],'.', color = col[i])
+            
+                centroid_colours = ['mx', 'gx', 'yx', 'rx', 'bx', 'kx', 'wx']
+                print("FINAL CENTROIDS")
+                print(self.centroids)
+                for i in range(self.k):
+                    centroid = self.centroids[i]
+                    plt.plot(centroid[0],centroid[1], centroid_colours[i])
+                # plt.scatter(self.centroids[:,0] ,self.centroids[:,1], color="green")
+                plt.title(title)
+                plt.show()
+
+                return
+        except Exception as error:
+            print(error)
+>>>>>>> Stashed changes
